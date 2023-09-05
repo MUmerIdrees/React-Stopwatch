1da43fdd271326c1dfad7e577f1d6f0d3eb967e3
MUmerIdrees <122514170+MUmerIdrees@users.noreply.github.com>
2023-09-05 23:03:19 +0500

    App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Btn from './components/Btn';
 import './App.css';
 
 function App() {
-  const [time, setTime] = useState({ms:0, s:50, m:59, h:0});
+  const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
   const [interv, setInterv] = useState();
   const [status, setStatus] = useState(0);
 
